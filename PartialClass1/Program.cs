860c6c09b582e7e21390493ba8f648befc8b3c77
ArmenHovhannisyan1998 <48031430+ArmenHovhannisyan1998@users.noreply.github.com>
2019-03-05 19:08:02 +0400

    Add project files.

---
+++ PartialClass1/Program.cs
@@ -0,0 +1,21 @@
+﻿using PartialClass1.PartialClass;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PartialClass1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+            person.Eat();
+            person.Move();
+
+            Console.ReadKey();
+        }
+    }
+}
